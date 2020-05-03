5e582ef28cb29ea0db6748abf2e3c010c92f0d6f
Richard Barbat <info@richard-barbat.de>
2020-05-03 02:51:46 +0200

    lol

--- Plinko Blaster/Pods/Target Support Files/RxSpriteKit/RxSpriteKit-umbrella.h
+++ Plinko Blaster/Pods/Target Support Files/RxSpriteKit/RxSpriteKit-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double RxRelayVersionNumber;
-FOUNDATION_EXPORT const unsigned char RxRelayVersionString[];
+FOUNDATION_EXPORT double RxSpriteKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char RxSpriteKitVersionString[];
 
